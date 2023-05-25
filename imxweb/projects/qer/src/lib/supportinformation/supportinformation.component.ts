2c75449960fd5e7213f8c87a631719f33b7f7737
SkianiD <dimitra.skiani@aspisid.com>
2023-05-25 10:49:35 +0000

    ts file of new component

---
+++ imxweb/projects/qer/src/lib/supportinformation/supportinformation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'imx-supportinformation',
+  templateUrl: './supportinformation.component.html',
+  styleUrls: ['./supportinformation.component.scss']
+})
+export class SupportinformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
