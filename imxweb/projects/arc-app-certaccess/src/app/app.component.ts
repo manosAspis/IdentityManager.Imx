e97209c9240e8500f0c784d563736866907400af
SkianiD <dimitra.skiani@aspisid.com>
2023-06-07 09:03:26 +0000

    import the component

--- imxweb/projects/arc-app-certaccess/src/app/app.component.ts
+++ imxweb/projects/arc-app-certaccess/src/app/app.component.ts
@@ -54,6 +54,7 @@ import { UserConfig } from 'imx-api-qer';
 import { AttestationConfig } from 'imx-api-att';
 import { RequestShopAlertComponent } from './configuration/request-shop-alert/request-shop-alert.component';
 import { RequestsService } from 'qer';
+import { CsvTableComponent } from 'projects/qer/src/lib/csv-table/csv-table.component';
 
 @Component({
   selector: 'imx-root',
@@ -148,7 +149,7 @@ export class AppComponent implements OnInit, OnDestroy {
         // Needs to close here when running in containers (auth skipped)
         this.splash.close();
 
-        this.checkSystemInfo();
+        this.checkSystemInfo(); 
 
         if (this.isBootstrapUser) {
           this.router.navigate(['data/explorer']);
@@ -275,7 +276,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.navigationItems.push(navItem);
     });
   }
-
+  
   private addNavMenuItem(text: string, url: string, items: { text: string; url: string }[]): void {
     const subNavItems: EuiTopNavigationItem[] = [];
     const subItemGeneration = new Promise<void>((resolve) => {
@@ -298,13 +299,13 @@ export class AppComponent implements OnInit, OnDestroy {
       this.addNavItem(text, url, false, subNavItems);
     });
   }
-
+  
   private setupNavigationItems(): void {
     this.navigationItems = [];
-
+  
     if (!this.isBootstrapUser) {
       this.addNavItem('#LDS#Home', 'dashboard');
-
+  
       if (this.qerUserConfig?.IsITShopEnabled) {
         this.addNavMenuItem('#LDS#Requests', '', [
           {
@@ -325,17 +326,19 @@ export class AppComponent implements OnInit, OnDestroy {
           },
         ]);
       }
-
-      const attestationMenuItems: { text: string; url: string }[] = [{
-        text: '#LDS#Menu Entry Pending attestations',
-        url: 'attestation/decision',
-      },
-      {
-        text: '#LDS#Menu Entry Attestation history',
-        url: 'attestation/history',
-      }];
-
-      if (canSeeAttestationPolicies(this.userGroups.map(item => item.Name))) {
+  
+      const attestationMenuItems: { text: string; url: string }[] = [
+        {
+          text: '#LDS#Menu Entry Pending attestations',
+          url: 'attestation/decision',
+        },
+        {
+          text: '#LDS#Menu Entry Attestation history',
+          url: 'attestation/history',
+        },
+      ];
+  
+      if (canSeeAttestationPolicies(this.userGroups.map((item) => item.Name))) {
         attestationMenuItems.push(
           {
             text: '#LDS#Menu Entry Attestation runs',
@@ -347,7 +350,7 @@ export class AppComponent implements OnInit, OnDestroy {
           }
         );
       }
-
+  
       if (this.attConfig?.IsAttestationEnabled) {
         this.addNavMenuItem('#LDS#Attestation', '', attestationMenuItems);
       }
@@ -355,71 +358,74 @@ export class AppComponent implements OnInit, OnDestroy {
 
     const userIsAdmin = this.userGroups.find((group) => {
       const groupName = group.Name ? group.Name.toUpperCase() : '';
-      return groupName === 'SIM_4_OPAADMIN'
-        || groupName === 'VI_4_NAMESPACEADMIN_ADS'
-        || groupName === 'SIM_BASEDATA_USERINTERFACE'
-        || groupName === 'AAD_4_NAMESPACEADMIN_AZUREAD';
+      return (
+        groupName === 'SIM_4_OPAADMIN' ||
+        groupName === 'VI_4_NAMESPACEADMIN_ADS' ||
+        groupName === 'SIM_BASEDATA_USERINTERFACE' ||
+        groupName === 'AAD_4_NAMESPACEADMIN_AZUREAD'
+      );
     });
-
+  
     const userIsShopAdmin = this.userGroups.find((group) => {
       const groupName = group.Name ? group.Name.toUpperCase() : '';
       return groupName === 'VI_4_ITSHOPADMIN_ADMIN';
     });
-
+  
     const userIsAttestationAdmin = this.userGroups.find((group) => {
       const groupName = group.Name ? group.Name.toUpperCase() : '';
       return groupName === 'VI_4_ATTESTATIONADMIN_ADMIN';
     });
-
+  
     if (!this.isBootstrapUser) {
 
       const respItems = [
         {
           text: '#LDS#Menu Entry System entitlement ownership',
           url: 'claimgroup',
-        }
+        },
       ];
-
+  
       if (this.isDelegationEnable) {
         respItems.push({ text: '#LDS#Menu Entry Delegation', url: 'delegation' });
       }
-
+  
       this.addNavMenuItem('#LDS#Responsibilities', '', respItems);
     }
-
+  
     const dataExplorerItems = [];
     const setupItems = [];
+  
     if (userIsAdmin) {
       dataExplorerItems.push({ text: '#LDS#Menu Entry Data Explorer', url: '/data/explorer' });
     }
-
+  
     if (!this.isBootstrapUser) {
-
+      
       if (userIsAdmin) {
         dataExplorerItems.push({ text: '#LDS#Menu Entry Reports', url: '/data/reports' });
       }
-
+  
       if (userIsAdmin || userIsShopAdmin) {
         setupItems.push({ text: '#LDS#Menu Entry Request shops', url: '/configuration/requests' });
       }
-
+  
       if (userIsAdmin) {
         setupItems.push({ text: '#LDS#Menu Entry Application roles', url: '/configuration/administratorgroups' });
       }
-
+  
       if (userIsAdmin || userIsShopAdmin) {
         setupItems.push({ text: '#LDS#Menu Entry Service categories', url: '/configuration/servicecategories' });
       }
-
+  
       if (userIsAdmin) {
         setupItems.push({ text: '#LDS#Menu Entry Feature availability', url: '/configuration/features' });
       }
-
+  
       if (userIsAttestationAdmin) {
         setupItems.push({ text: '#LDS#Menu Entry Attestation schedules', url: '/configuration/schedules' });
       }
     }
-
+  
     if (dataExplorerItems.length > 0) {
       this.addNavMenuItem('#LDS#Data administration', '', dataExplorerItems);
     }
@@ -427,18 +433,17 @@ export class AppComponent implements OnInit, OnDestroy {
     if (setupItems.length > 0) {
       this.addNavMenuItem('#LDS#Setup', '', setupItems);
     }
-  }
 
+  }
+  
   private async checkSystemInfo(): Promise<void> {
     const seenUpgradeNotice = sessionStorage.getItem('ARC-Governance_UpgradeNoticeSeen');
-
+  
     if (!seenUpgradeNotice) {
       const info = await this.systemInfo.get();
-
-      this.isDelegationEnable = info
-        && info.PreProps.includes('ITSHOP')
-        && info.PreProps.includes('DELEGATION');
-
+  
+      this.isDelegationEnable = info && info.PreProps.includes('ITSHOP') && info.PreProps.includes('DELEGATION');
+  
       if (info && info.UpdatePhase > 0) {
         this.translate
           .get('#LDS#Starling CertAccess is currently updating and will soon be unavailable until this process is complete.')
@@ -446,7 +451,7 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     }
   }
-
+  
   private openUpgradeAlert(message: string): void {
     setTimeout(() => {
       this.alertBanner.open({
@@ -455,12 +460,12 @@ export class AppComponent implements OnInit, OnDestroy {
         message,
       });
     }, 500);
-
+  
     this.alertBannerDismissed$ = this.alertBanner.userDismissed.subscribe(() =>
       sessionStorage.setItem('ARC-Governance_UpgradeNoticeSeen', 'true')
     );
   }
-
+  
   /**
    * Update the language in the lang attribute of the html element.
    */
@@ -470,3 +475,4 @@ export class AppComponent implements OnInit, OnDestroy {
     document.getElementsByTagName('html')?.[0]?.attributes.setNamedItem(lang);
   }
 }
+  
