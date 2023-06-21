fcabe0c7d9ad652d9fd81fe1bad82fcf4c6e81fe
giannoutsosBarkas <dimitris.giannoutsos@aspisid.com>
2023-06-21 09:24:26 +0000

    CSV-Importer / Task 5

--- imxweb/projects/qer/src/lib/qer.module.ts
+++ imxweb/projects/qer/src/lib/qer.module.ts
@@ -61,6 +61,7 @@ import { TilesModule } from './tiles/tiles.module';
 import { UserModule } from './user/user.module';
 import { ShoppingCartValidationDetailModule } from './shopping-cart-validation-detail/shopping-cart-validation-detail.module';
 import { RoleMembershipsModule } from './role-management/role-memberships/role-memberships.module';
+import { CsvImporterComponent } from './csv-importer/csv-importer.component';
 
 export function initConfig(config: QerService): () => Promise<any> {
   return () =>
@@ -78,6 +79,12 @@ const routes: Routes = [
     component: StartComponent,
     canActivate: [RouteGuardService],
     resolve: [RouteGuardService],
+  },
+  {
+    path: 'csv-importer',
+    component: CsvImporterComponent,
+    canActivate: [RouteGuardService],
+    resolve: [RouteGuardService]
   },
 ];
 
