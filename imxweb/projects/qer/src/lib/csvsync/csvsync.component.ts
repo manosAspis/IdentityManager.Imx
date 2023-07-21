950817cbce1a3d255dc6161bcdd0cfb05923bad5
unknown <OneIM@CONTOSO.COM>
2023-07-21 17:09:27 +0000

    1

--- imxweb/projects/qer/src/lib/csvsync/csvsync.component.ts
+++ imxweb/projects/qer/src/lib/csvsync/csvsync.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, AfterViewInit } from '@angular/core';
 import { EuiSidesheetService } from '@elemental-ui/core';
-import { CsvmappingComponent } from './csvmapping/csvmapping.component';
-
 import { HeaderService } from './csvmapping/header.service';
 import { CsvDataService } from './csvmapping/csvdata.service';
 import { QerService } from '../qer.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'imx-csvsync',
   templateUrl: './csvsync.component.html',
   styleUrls: ['./csvsync.component.scss']
 })
-export class CsvsyncComponent implements OnInit {
+export class CsvsyncComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  csvDataSource: MatTableDataSource<any> = new MatTableDataSource();
   @Input() csvData: any[] = [];
   fileLoaded: boolean = false;
   columnMapping: { [key: string]: string } = {};
@@ -38,36 +39,83 @@ export class CsvsyncComponent implements OnInit {
     console.log(this.CsvImporter)
   }
 
+  ngAfterViewInit() {
+    this.csvDataSource.paginator = this.paginator;
+  }
+
   onFileChange(event: any) {
     const file = event.target.files[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const data = reader.result as string;
-        const lines = data.split('\n');
-        this.headers = lines[0].split(',');
-        for (let i = 1; i < lines.length; i++) {
-          const row = lines[i].split(',');
-          if (row.length > 1) {
-            this.csvData.push(row);
-          }
-        }
-        this.csvDataService.setCsvData(this.csvData);
-        this.fileLoaded = true;
-      };
-      reader.readAsText(file);
-
+      this.processFile(file);
     }
   }
 
+  onDragOver(event: any) {
+    event.preventDefault();
+    if (event.target) {
+      event.target.classList.add('dragover');
+    }
+  }
+
+  onDragLeave(event: any) {
+    event.preventDefault();
+    if (event.target) {
+      event.target.classList.remove('dragover');
+    }
+  }
+
+  triggerFileUpload() {
+    const fileInput = document.getElementById('csv-file') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
+  onDrop(event: any) {
+    event.preventDefault();
+    let files = event.dataTransfer.files;
+    if (files.length > 0) {
+      let file = files[0];
+      if (file.type.match(/.csv/)) {
+        this.processFile(file);
+      }
+    }
+  }
+
+  private processFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const data = reader.result as string;
+      const lines = data.split('\n');
+      this.headers = lines[0].split(',');
+      for (let i = 1; i < lines.length; i++) {
+        const row = lines[i].split(',');
+        if (row.length > 1) {
+            this.csvData.push(row);
+        }
+      }
+      this.csvDataSource.data = this.csvData;
+      this.csvDataService.setCsvData(this.csvData);
+      this.fileLoaded = true;
+    };
+    reader.readAsText(file);
+  }
 
   onCellValueChange(event: any, rowIndex: number, cellIndex: number): void {
     this.csvData[rowIndex][cellIndex] = event.target.value;
   }
 
+  validate() {}
+
+  replaceCsv() {
+    this.fileLoaded = false;
+    this.csvData = [];
+    this.headers = [];
+    this.csvDataSource.data = [];
+  }
 
   importToDatabase() {
-    this.headerService.setHeaders(this.headers);
+    /*this.headerService.setHeaders(this.headers);
     const sideSheetRef = this.sideSheet.open(CsvmappingComponent, {
       title: 'Column Mapping',
       headerColour: 'iris-blue',
@@ -85,6 +133,6 @@ export class CsvsyncComponent implements OnInit {
       if (result) {
         this.columnMapping = result.columnMapping;
       }
-    });
+    });*/
   }
 }
