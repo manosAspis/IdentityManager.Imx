05770d476970340a05c33a22f3149d14a5099b8f
Grzegorz Wojciak <grzegorz.wojciak@ing.com>
2023-09-22 09:37:15 +0200

    Add API support

--- imxweb/projects/qer/src/lib/support/popup-support-window/popup-support-window.component.ts
+++ imxweb/projects/qer/src/lib/support/popup-support-window/popup-support-window.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { Inject } from '@angular/core';
+import { MethodDescriptor, TimeZoneInfo } from 'imx-qbm-dbts';
+import { AppConfigService, AuthenticationService } from 'qbm';
+
+export interface PeriodicElement {
+  contactInfo: string;
+  headCoe: string;
+  serviceNow: string;
+  confluence: string;
+}
+
+let ELEMENT_DATA: PeriodicElement[] = [
+  {headCoe: '', contactInfo: '', serviceNow: '', confluence: ''},
+  
+];
 
 
 @Component({
@@ -8,18 +22,49 @@ import { Inject } from '@angular/core';
   templateUrl: './popup-support-window.component.html',
   styleUrls: ['./popup-support-window.component.scss']
 })
-export class PopupSupportWindowComponent implements OnInit {
 
+
+
+export class PopupSupportWindowComponent implements OnInit {
+  dataSource = ELEMENT_DATA;
   constructor(
     private dialogRef: MatDialogRef<PopupSupportWindowComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private readonly config: AppConfigService,
+    private readonly authentication: AuthenticationService
   ) { }
 
   ngOnInit(): void {
+    //alert(this.getFeatureConfigDescriptor);
+    this.getCustom();
   }
+  
 
   closeWindow() {
     localStorage.setItem('newsLast', this.data.newsDBdate);
     this.dialogRef.close();
+  }
+
+  public async getCustom(): Promise<String> {
+    const data = await this.config.apiClient.processRequest(this.getFeatureConfigDescriptor());
+    //ELEMENT_DATA = [data];
+    //this.dataSource = ELEMENT_DATA;
+    alert(data);
+    return data;
+   }
+
+  private getFeatureConfigDescriptor(): MethodDescriptor<String> {
+    const parameters = [];
+    return {
+      path: `/portal/GetNews`,
+      parameters,
+      method: 'GET',
+      headers: {
+        'imx-timezone': TimeZoneInfo.get(),
+      },
+      credentials: 'include',
+      observe: 'response',
+      responseType: 'json',
+    };
   }
 }
