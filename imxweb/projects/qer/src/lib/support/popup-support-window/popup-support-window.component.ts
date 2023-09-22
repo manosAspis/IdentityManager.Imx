a710d531652453f56a6c4463abd95404f8f3e157
gwojciak <145326789+gwojciak@users.noreply.github.com>
2023-09-22 09:47:06 +0200

    V82 dialog api (#52)
    * Update v82 (#75)
    
    Automated sync from source branch v82
    ---------
    
    Co-authored-by: imx-sync-bot <imx-sync-bot@oneidentity>
    Co-authored-by: Hanno Bunjes <hanno.bunjes@oneidentity.com>
    
    * package-lock.json updated
    
    * removed duplicated entry in package-lock.json
    
    * created new newsletter component and feature
    
    * Added imports and dynamic Ngmodule import
    
    * Added newsletter route
    
    * created new Other page menu
    
    * created support module + 3 components + imports
    
    * Support menu created
    
    * removed newsletter module
    
    * removed Support page link
    
    * added ROOT_SUPPORT style
    
    * created popup window component
    
    * localstorage support for dialog
    
    * Add API support
    
    * Fix to API support
    
    ---------
    
    Co-authored-by: Hanno Bunjes <hanno.bunjes@quest.com>
    Co-authored-by: imx-sync-bot <imx-sync-bot@oneidentity>
    Co-authored-by: Hanno Bunjes <hanno.bunjes@oneidentity.com>
    Co-authored-by: Patryk-jkc <80311836+Patryk-jkc@users.noreply.github.com>
    Co-authored-by: Adam <adamo156@gmail.com>

--- imxweb/projects/qer/src/lib/support/popup-support-window/popup-support-window.component.ts
+++ imxweb/projects/qer/src/lib/support/popup-support-window/popup-support-window.component.ts
@@ -2,24 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { Inject } from '@angular/core';
 
+import { MethodDescriptor, TimeZoneInfo } from 'imx-qbm-dbts';
+import { AppConfigService, AuthenticationService } from 'qbm';
 
 @Component({
   selector: 'ccc-popup-support-window',
   templateUrl: './popup-support-window.component.html',
   styleUrls: ['./popup-support-window.component.scss']
 })
-export class PopupSupportWindowComponent implements OnInit {
 
+export class PopupSupportWindowComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<PopupSupportWindowComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private readonly config: AppConfigService,
+    private readonly authentication: AuthenticationService
   ) { }
 
   ngOnInit(): void {
+    this.getCustom();
   }
+  
 
   closeWindow() {
     localStorage.setItem('newsLast', this.data.newsDBdate);
     this.dialogRef.close();
   }
+
+  public async getCustom(): Promise<String> {
+    const data = await this.config.apiClient.processRequest(this.getFeatureConfigDescriptor());
+    alert(data);
+    return data;
+   }
+
+  private getFeatureConfigDescriptor(): MethodDescriptor<String> {
+    const parameters = [];
+    return {
+      path: `/portal/GetNews`,
+      parameters,
+      method: 'GET',
+      headers: {
+        'imx-timezone': TimeZoneInfo.get(),
+      },
+      credentials: 'include',
+      observe: 'response',
+      responseType: 'json',
+    };
+  }
+
 }
