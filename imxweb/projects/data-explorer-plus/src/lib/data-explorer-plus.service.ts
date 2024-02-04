ded3ebdcaee6c0d6e6d9e73ff61ee3df672f7847
unknown <OneIM@contoso.com>
2024-02-04 18:38:44 +0000

    menu try

--- imxweb/projects/data-explorer-plus/src/lib/data-explorer-plus.service.ts
+++ imxweb/projects/data-explorer-plus/src/lib/data-explorer-plus.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Router, Route } from '@angular/router';
-
-import { MenuItem, MenuService } from 'qbm';
+import { MenuItem, MenuService, AppConfigService, AuthenticationService } from 'qbm';
+import { OverlayRef } from '@angular/cdk/overlay';
+import { EuiLoadingService } from '@elemental-ui/core';
+import { SystemInfo } from 'imx-api-qbm';
+import { MethodDescriptor, TimeZoneInfo } from 'imx-qbm-dbts';
 
 
 @Injectable({ providedIn: 'root' })
 export class DataExplorerPlusService {
+
+  public systemInfo: SystemInfo;
+  public viewReady: boolean;
+  public userUid: string;
+  dataSource: string[] = [];
+  explores: number = 0;
+
   constructor(
-    private readonly router: Router,
+    public readonly router: Router,
+    private readonly busyService: EuiLoadingService,
+    private readonly config: AppConfigService,
+    private readonly authentication: AuthenticationService,
     private readonly menuService: MenuService,
   ) {
-    this.setupMenu();
+    this.authentication.onSessionResponse.subscribe({
+      next: sessionState => {
+        if (sessionState?.IsLoggedOut === false) {
+          this.setupMenuAfterAuthentication();
+        }
+      }
+    });
   }
 
   public onInit(routes: Route[]): void {
     this.addRoutes(routes);
+    let overlayRef: OverlayRef;
+    setTimeout(() => overlayRef = this.busyService.show(), 0);
+
+    try {
+      this.viewReady = true;
+    } finally {
+      setTimeout(() => this.busyService.hide(overlayRef), 0);
+    }
   }
 
   private addRoutes(routes: Route[]): void {
@@ -25,26 +52,61 @@ export class DataExplorerPlusService {
     this.router.resetConfig(config);
   }
 
-  private setupMenu(): void {
-   if (1===1) {
-    this.menuService.addMenuFactories((preProps: string[], features: string[]) => {
-
-        const menu: MenuItem = {
-          id: 'ROOT_DataExplorerPlus',
-          title: '#LDS#Data Explorer +',
-          sorting: '20',
-          items: [
-            {
-              id: 'DATA-EXPLORER-PLUS_test',
-              route: 'data-explorer-plus',
-              title: '#LDS#Test menu item',
-              sorting: '20-10',
-            }
-          ],
-        };
-        return menu;
-      }
-    );
+  calculateKeyCounts(): void {
+    if (this.dataSource && this.dataSource.length > 0) {
+      this.explores = this.dataSource.reduce((count, item) => {
+        count += Object.keys(item).length;
+        return count;
+      }, 0);
+    }
   }
+
+
+
+  public async ExplorerList(): Promise<void> {
+    const explorers = await this.config.apiClient.processRequest<string[]>(this.GetExplorers());
+    console.log(explorers);
+    this.dataSource = explorers;
+  }
+
+
+  private GetExplorers(): MethodDescriptor<void> {
+    const parameters = [];
+    return {
+      path: `/portal/dataexplorerplus/configparms`,
+      parameters,
+      method: 'GET',
+      headers: {
+        'imx-timezone': TimeZoneInfo.get(),
+      },
+      credentials: 'include',
+      observe: 'response',
+      responseType: 'json',
+    };
+  }
+
+  private setupMenuAfterAuthentication(): void {
+    this.ExplorerList();
+    this.calculateKeyCounts();
+    if (1===1) {
+      this.menuService.addMenuFactories((preProps: string[], features: string[]) => {
+
+          const menu: MenuItem = {
+            id: 'ROOT_DataExplorerPlus',
+            title: '#LDS#Data Explorer +',
+            sorting: '20',
+            items: [
+              {
+                id: 'DATA-EXPLORER-PLUS_test',
+                route: 'data-explorer-plus',
+                title: '#LDS#Test menu item',
+                sorting: '20-10',
+              }
+            ],
+          };
+          return menu;
+        }
+      );
+    }
 }
 }
